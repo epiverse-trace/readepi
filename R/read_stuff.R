0bd5445b1d110723b5dca3e84bb2560d1209441a
Thibaut Jombart <thibautjombart@gmail.com>
2022-08-24 14:36:36 +0200

    adding documentation and read_stuff

---
+++ R/read_stuff.R
@@ -0,0 +1,14 @@
+#' Example of a function to read stuff
+#'
+#' This function reads stuff into R. More details...
+#'
+#' @param x the path to the file to read stuff from
+#'
+#' @author Thibaut Jombart
+#'
+#' @export
+#'
+
+read_stuff <- function(x) {
+  x
+}
