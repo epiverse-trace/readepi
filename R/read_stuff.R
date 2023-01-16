9fe914a781d27e6932747a7485c1deb544782220
Karim Mane <karimmane@iMAC-HOTDESKUSER.local>
2023-01-16 15:44:25 +0000

    update package structure

--- R/read_stuff.R
+++ R/read_stuff.R
@@ -11,11 +11,9 @@
 #' @examples
 #' some_path <- "path to your file"
 #' read_stuff(some_path)
-
 read_stuff <- function(x) {
   # check inputs
   checkmate::assertCharacter(x, len = 1L)
-
   
   # do stuff
   x
@@ -23,21 +21,18 @@ read_stuff <- function(x) {
 
 
 
-
-
-
-#' function to read data from all supported file types in the `rio` package. 
+#' function to read data from all supported file types in the `rio` package.
 #' @param file.path the path to the file to be read. When several files need to be imported from a directory, this should be the path to that directory
-#' @param sep the separator between the columns in the file. This is only required for space-separated files. 
+#' @param sep the separator between the columns in the file. This is only required for space-separated files
 #' @param format a string used to specify the file format. This is useful when a file does not have an extension, or has a file extension that does not match its actual type
 #' @param which a string used to specify the name of the excel sheet to import
 #' @param pattern when specified, only files with this suffix will be imported from the specified directory
 #' @returns  a data frame (import from file) or a list (import several files from directory)
-#' @examples data = read_from_file(file.path = "/path/to/test.txt", sep=NULL, format=NULL, which=NULL, pattern=NULL)
+#' @examples data = read_from_file(file.path = system.file("extdata", "test.txt", package = "readepi"), sep=NULL, format=NULL, which=NULL, pattern=NULL)
 #' @export
 read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=NULL){
   # check if the file exists
-  if(!file.exists(file.path) & !dir.exists(file.path)){ 
+  if(!file.exists(file.path) & !dir.exists(file.path)){
     stop(file.path, " No such file or directory!")
   }
   
@@ -63,10 +58,10 @@ read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=
 
 # read_from_file = function(file.path, sep=NULL, sheet.name=NULL){
 #   # check if the file exists
-#   if(!file.exists(file.path)){ 
+#   if(!file.exists(file.path)){
 #     stop(file.path, " not found!")
 #   }
-#   
+
 #   # reading the file
 #   file.extension = getExtension(file.path)
 #   if(file.extension %in% c("xlsx","xls")){
@@ -75,12 +70,11 @@ read_from_file = function(file.path, sep=NULL, format=NULL, which=NULL, pattern=
 #   }else if(file.extension %in% c("json","JSON")){
 #     data = jsonlite::fromJSON(file.path, simplifyDataFrame=TRUE)
 #   }else if(file.extension %in% c("xml","XML")){
-#     
+#
 #   }else{
 #     if(!is.null(sep)) data = fread(file.path, sep = sep, nThread = 4)
 #     else data = fread(file.path, nThread = 4)
 #   }
-#   as.data.table(data)
 # }
 
 # function to return the file extension
@@ -98,12 +92,13 @@ getExtension = function(file.path){
 #' @param database.name the name of the database that contains the table from which the data should be pulled
 #' @param table.name the name of the target table
 #' @param driver.name the name of the MS driver. use `odbc::odbcListDrivers()` to display the list of installed drivers
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
+#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.
 #' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.  
+#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.
 #' @returns a data frame
+#' @examples data = read_from_ms_sql_server(user="kmane", password="Dakabantang@KD23", host="robin.mrc.gm", port=1433, database.name="IBS_BHDSS", table.name="dss_events", driver.name="ODBC Driver 17 for SQL Server")
 #' @export
-#' @examples data = read_from_relationalDB(user="kmane", password="****", host="robin.mrc.gm", port=1433, database.name="mrcg_db", table.name="test", driver.name="ODBC Driver 17 for SQL Server")
+#' @importFrom magrittr %>%
 read_from_ms_sql_server = function(user, password, host, port, database.name, driver.name, table.name, records=NULL, fields=NULL, id.position=1){
   # reading in user credentials
   # credentials = readMSsqlCredentials(credentials.file)
@@ -165,8 +160,7 @@ readMSsqlCredentials = function(credentials.file){
   if(!file.exists(credentials.file)){
     stop("Could not find ",credentials.file)
   }
-  credentials = data.table::fread(credentials.file, 
-                                  sep = "\t")
+  credentials = data.table::fread(credentials.file, sep = "\t")
   if(ncol(credentials)!=4){
     stop("credential file should be tab-separated file with 4 columns.")
   }
@@ -180,14 +174,14 @@ readMSsqlCredentials = function(credentials.file){
 }
 
 #' function to read data from REDCap
-#' @param uri the URI of the server   
+#' @param uri the URI of the server
 #' @param token the user-specific string that serves as the password for a project
 #' @param project.id the name of the project you want to import data from
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.   
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
-#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.   
-#' @examples redcap.data = read_from_redcap(uri, token, project.id, id_position=1, records=NULL, fields=NULL)
-#' @returns a list with 2 data frames: the data of interest and the metadata associated to the data.      
+#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.
+#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.
+#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.
+#' @examples redcap.data = read_from_redcap(uri="https://redcap.mrc.gm:8443/redcap/api/", token="9D71857D60F4016AB7BFFDA65970D737", project.id="Pats__Covid_19_Cohort_1_Screening", id.position=1, records=NULL, fields=NULL)
+#' @returns a list with 2 data frames: the data of interest and the metadata associated to the data.
 #' @export
 read_from_redcap = function(uri, token, project.id, id.position=1L, records=NULL, fields=NULL){
   # importing the credentials
@@ -230,8 +224,8 @@ read_from_redcap = function(uri, token, project.id, id.position=1L, records=NULL
   
   # checking whether the importing was successful and extract the desired records and columns
   if(redcap.data$success & metadata$success){
-    data = as.data.table(redcap.data$data)
-    meta = as.data.table(metadata$data)
+    data = redcap.data$data
+    meta = metadata$data
   }
   
   # return the imported data
@@ -241,12 +235,17 @@ read_from_redcap = function(uri, token, project.id, id.position=1L, records=NULL
 
 
 #' function to read credentials from a configuration file
+#' @param file.path the path to the file with the user-specific credential details for the projects of interest.
+#' @param project.id for relational DB, this is the name of the database that contains the table from which the data should be pulled. Otherwise, it is the project ID you were given access to.
+#' @returns  a list with the user credential details.
+#' @examples credentials = readCredentials(file.path=system.file("extdata", "test.ini", package = "readepi"), project.id="Pats__Covid_19_Cohort_1_Screening")
+#' @export
 readCredentials = function(file.path, project.id){
   if(!file.exists(file.path)){
     stop("Could not find ",file.path)
   }
-  credentials = data.table::fread(file.path, 
-                                  sep = "\t")
+  
+  credentials = data.table::fread(file.path, sep = "\t")
   if(ncol(credentials)!=7){
     stop("credential file should be tab-separated file with 7 columns.")
   }
@@ -270,46 +269,54 @@ readCredentials = function(file.path, project.id){
 }
 
 
+#' function to display the structure of the credentials file
+#' @export
+#' @examples show_example_file()
+show_example_file = function(){
+  example.data = data.table::fread(system.file("extdata", "test.ini", package = "readepi"))
+  print(example.data)
+}
+
+
 
 #' Function to import epidemiology-related data from different sources into R
-#' @description the function is a wrapper around numerous functions that are used to import data stored in database management systems (DBMS) including both relational and NoSQL databases. It also contains functions to import data from common file types such as csv, txt, xlsx, xml, json, etc.
-#' @param credentials.file the path to the file with the user-specific credential details for the projects of interest. It is required when importing data from DBMS. This is a tab-delimited file with the following columns:   
+#' @description the function allows import of data stored in database management systems (DBMS) including both relational and NoSQL databases. It also contains functions to import data from common file types such as csv, txt, xlsx, xml, json, etc.
+#' @param credentials.file the path to the file with the user-specific credential details for the projects of interest. It is required when importing data from DBMS. This is a tab-delimited file with the following columns:
 #' \enumerate{
 #'   \item user_name: the user name
 #'   \item password: the user password (for REDCap, this corresponds to the **token** that serves as password to the project)
-#'   \item host_name: the host name (for relational DB) or the URI (for NoSQL DB such as REDCap)
-#'   \item project_id: the project ID or the name of the database you are access to. 
+#'   \item host_name: the host name (for MS SQL servers) or the URI (for REDCap)
+#'   \item project_id: the project ID or the name of the database you are access to.
 #'   \item comment: a summary description about the project or database of interest
 #'   \item dbms: the name of the DBMS: 'redcap' or 'REDCap' when reading from REDCap, 'sqlserver' or 'SQLServer' when reading from MS SQLServer
 #'   \item port: the port ID (used for MS SQLServers)
 #'   }
 #' @param file.path the path to the file to be read. When several files need to be imported from a directory, this should be the path to that directory
-#' @param sep the separator between the columns in the file. This is only required for space-separated files. 
+#' @param sep the separator between the columns in the file. This is only required for space-separated files.
 #' @param format a string used to specify the file format. This is useful when a file does not have an extension, or has a file extension that does not match its actual type
 #' @param which a string used to specify which objects should be extracted (the name of the excel sheet to import)
 #' @param pattern when specified, only files with this suffix will be imported from the specified directory
 #' @param project.id for relational DB, this is the name of the database that contains the table from which the data should be pulled. Otherwise, it is the project ID you were given access to.
 #' @param driver.name the name of the MS driver. use `odbc::odbcListDrivers()` to display the list of installed drivers
 #' @param table.name the name of the target table
-#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.   
-#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.   
-#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.  
-#' @example data = readepi(credentials.file, project.id="test")
-#' @returns a list with 2 data frames (data and metadata) when reading from REDCap. A data frame otherwise.   
+#' @param records a vector or a comma-separated string of subset of subject IDs. When specified, only the records that correspond to these subjects will be imported.
+#' @param fields a vector or a comma-separated string of column names. If provided, only those columns will be imported.
+#' @param id.position the column position of the variable that unique identifies the subjects. This should only be specified when the column with the subject IDs is not the first column. default is 1.
+#' @examples data = readepi(credentials.file=system.file("extdata", "test.ini", package = "readepi"), project.id="Pats__Covid_19_Cohort_1_Screening")
+#' @returns a list with 2 data frames (data and metadata) when reading from REDCap. A data frame otherwise.
 #' @export
-#' 
-readepi = function(credentials.file=NULL, 
-                    file.path=NULL, 
-                    sep=NULL, 
-                    format=NULL, 
-                    which=NULL,
-                    pattern=NULL,
-                    project.id = NULL,
-                    driver.name=NULL, 
-                    table.name=NULL,
-                    records=NULL, 
-                    fields=NULL,
-                    id.position=1){
+readepi = function(credentials.file=NULL,
+                   file.path=NULL,
+                   sep=NULL,
+                   format=NULL,
+                   which=NULL,
+                   pattern=NULL,
+                   project.id = NULL,
+                   driver.name=NULL,
+                   table.name=NULL,
+                   records=NULL,
+                   fields=NULL,
+                   id.position=1){
   
   # some check points
   if(!is.null(credentials.file) & !is.null(file.path)){
@@ -325,17 +332,17 @@ readepi = function(credentials.file=NULL,
   if(!is.null(credentials.file)){
     credentials = readCredentials(credentials.file, project.id)
     if(credentials$dbms %in% c('redcap','REDCap')){
-      res = read_from_redcap(uri=credentials$host, token=credentials$pwd, project.id=credentials$project, id.position=id.position, 
+      res = read_from_redcap(uri=credentials$host, token=credentials$pwd, project.id=credentials$project, id.position=id.position,
                              records=records, fields=fields)
     }else if(credentials$dbms %in% c('sqlserver','SQLServer')){
-      res = read_from_ms_sql_server(user=credentials$user, password=credentials$pwd, host=credentials$host, port=credentials$port, 
+      res = read_from_ms_sql_server(user=credentials$user, password=credentials$pwd, host=credentials$host, port=credentials$port,
                                     database.name=credentials$project, table.name=table.name, driver.name=driver.name, records=records,
                                     fields=fields)
     }
   }
   
   res
-} 
+}
 
 
 
--- README.md
+++ README.md
@@ -23,21 +23,171 @@ You can install the development version of readepi from
 
 ``` r
 # install.packages("devtools")
-devtools::install_github("epiverse-trace/readepi")
+#devtools::install_github("epiverse-trace/readepi")
+suppressPackageStartupMessages(library(readepi))
 ```
 
-## Example
+# Reading data from file or directory
 
-These examples illustrate some of the current functionalities:
+The **readepi()** function can read read data from various source
+including various file formats. It is based on the **rio** R package,
+hence can import data from all file format supported by that one. See
+[here](https://cran.r-project.org/web/packages/rio/vignettes/rio.html)
+for more details about the file formats allowed by `rio`.  
+Additionally, it can read in data from a space-separated file or read
+specific or all files from a specified directory. When reading data from
+file or directory, the function expects the following arguments:  
+`file.path`: the path to the file to be read. When several files need to
+be imported from a directory, this should be the path to that
+directory,  
+`sep`: the separator between the columns in the file. This is only
+required for space-separated files,  
+`format`: a string used to specify the file format. This is useful when
+a file does not have an extension, or has a file extension that does not
+match its actual type,  
+`which`: which a string used to specify which objects should be
+extracted (e.g. the name of the excel sheet to import),  
+`pattern`: when specified, only files with this suffix will be imported
+from the specified directory.  
+The function will return either a data frame (if import from file) or a
+list of data frames (if several files were imported from a directory).
+
+## importing data from JSON file
 
 ``` r
-library(readepi)
+file = system.file("extdata", "test.json", package = "readepi")
+data = readepi(file.path = file)
+```
 
-# example of read_stuff():
-# (a placeholder function returning its own argument)
-path_to_file <- "some_path_here"
-read_stuff(path_to_file)
-#> [1] "some_path_here"
+## importing data from excel file with 2 sheets
+
+here we are importing from the second excel sheet
+
+``` r
+file = system.file("extdata", "test.xlsx", package = "readepi")
+data = readepi(file.path = file, which = "Sheet2")
+```
+
+## importing data from several files in a directory
+
+``` r
+# reading all files in the given directory
+dir.path = "inst/extdata"
+data = readepi(file.path = dir.path)
+
+# reading only txt files
+data = readepi(file.path = dir.path, pattern = "txt")
+```
+
+# Reading data from relational database management systems (RDBMS): HDSS, EMRS, REDCap
+
+Research data are usually stored in either relational databases or NoSQL
+databases. At the MRCG, project data are stored in relational databases.
+The HDSS and EMRS host databases that run under MS SQL Server, while
+REDCap (that uses an EAV schema) run under a MySQL server.  
+To import data from a HDSS or EMRS (MS SQL Server) into R, some
+dependencies need to be installed first.
+
+## installation of dependencies
+
+If you are using a Unix-based system, you will need to install the MS
+ODBC driver that is compatible with the version of the target MS SQL
+server. For **SQL server 2019, version 15.0**, we installed **ODBC
+Driver 17 for SQL Server** on the mac OS. This is compatible with the
+MRCG test server `robin.mrc.gm`. Details about how to install a driver
+can be found
+[here](https://learn.microsoft.com/en-us/sql/connect/odbc/linux-mac/install-microsoft-odbc-driver-sql-server-macos?view=sql-server-ver15).  
+Once installed, the list of drivers can be displayed in R using:
+`odbc::odbcListDrivers()`.  
+It is important to view the data that is stored in the MS SQL server. I
+recommend to install a GUI such as `Azure Data Studio`.
+
+## importing data from RDBMS into R
+
+For testing purpose, we were given access to the test server
+`robin.mrc.gm`. Users should be granted with read access to be able to
+pull data from the test server.  
+The **read_epi()** function can read data from the above RDBMS. It
+expects the following arguments:  
+`credentials.file`: the path to the file with the user-specific
+credential details for the projects of interest. This is a tab-delimited
+file with the following columns:
+
+- user_name: the user name,  
+- password: the user password (for REDCap, this corresponds to the
+  **token** that serves as password to the project),  
+- host_name: the host name (for HDSS and EMRS) or the URI (for
+  REDCap),  
+- project_id: the project ID (for REDCap) or the name of the database
+  (for HDSS and EMRS) you are access to,  
+- comment: a summary description about the project or database of
+  interest,  
+- dbms: the name of the DBMS: ‘redcap’ or ‘REDCap’ when reading from
+  REDCap, ‘sqlserver’ or ‘SQLServer’ when reading from MS SQL Server,  
+- port: the port ID (used for MS SQL Servers only).  
+  `project.id` for relational DB, this is the name of the database that
+  contains the table from which the data should be pulled. Otherwise, it
+  is the project ID you were given access to. Note that this should be
+  similar to the value of the **project_id** field in the credential
+  file.  
+  `driver.name` the name of the MS driver (only for HDSS and EMRS). use
+  `odbc::odbcListDrivers()` to display the list of installed drivers,  
+  `table.name`: the name of the target table (only for HDSS and EMRS),  
+  `records`: a vector or a comma-separated string of a subset of subject
+  IDs. When specified, only the records that correspond to these
+  subjects will be imported,  
+  `fields`: a vector or a comma-separated string of column names. If
+  provided, only those columns will be imported,  
+  `id.position`: the column position of the variable that unique
+  identifies the subjects. This should only be specified when the column
+  with the subject IDs is not the first column. default is 1.
+
+The function returns a list with 2 data frames (data and metadata) when
+reading from REDCap. A data frame otherwise.
+
+### importing from REDCap
+
+we were given access to the
+\*\*Pats\_\_Covid_19_Cohort_1\_Screening\*\*.
+
+``` r
+# display the structure of the credentials file
+show_example_file()
+# credentials.file = system.file("extdata", "test.ini", package = "readepi")
+
+# reading all fields and records the project
+data = readepi(credentials.file, project.id="Pats__Covid_19_Cohort_1_Screening")
+project.data = data$data
+project.metadeta = data$metadata
+
+# reading specified fields and all records from the project
+fields = c("day_1_q_ran_id","redcap_event_name","day_1_q_1a","day_1_q_1b","day_1_q_1c","day_1_q_1","day_1_q_2","day_1_q_3","day_1_q_4","day_1_q_5")
+data = readepi(credentials.file, project.id="Pats__Covid_19_Cohort_1_Screening", fields = fields)
+
+# reading specified records and all fields from the project
+records = c("C10001/3","C10002/1","C10003/7","C10004/5","C10005/9","C10006/8","C10007/6","C10008/4","C10009/2","C10010/1")
+data = readepi(credentials.file, project.id="Pats__Covid_19_Cohort_1_Screening", records =  records)
+
+# reading specified records and fields from the project
+data = readepi(credentials.file, project.id="Pats__Covid_19_Cohort_1_Screening", records =  records, fields = fields)
+```
+
+### importing from HDSS or EMRS
+
+we were given access to the **IBS_BHDSS** database.
+
+``` r
+# reading all fields and all records from the `dss_events` table
+data = readepi(credentials.file, project.id="IBS_BHDSS", driver.name = "ODBC Driver 17 for SQL Server", table.name = "dss_events")
+
+# reading specified fields and all records from the table
+data = readepi(credentials.file, project.id="IBS_BHDSS", driver.name = "ODBC Driver 17 for SQL Server", table.name = "dss_events", fields = fields)
+
+# reading specified records and all fields from the table
+data = readepi(credentials.file, project.id="IBS_BHDSS", driver.name = "ODBC Driver 17 for SQL Server", table.name = "dss_events", records = records)
+
+# reading specified fields and records from the table
+data = readepi(credentials.file, project.id="IBS_BHDSS", driver.name = "ODBC Driver 17 for SQL Server", table.name = "dss_events", records = records, fields = fields)
 ```
 
 ## Development
@@ -56,7 +206,8 @@ requests](https://github.com/epiverse-trace/readepi/pulls).
 
 Contributors to the project include:
 
-  - Thibaut Jombart (author)
+- Karim Mané (author)
+- Thibaut Jombart (author)
 
 ### Code of Conduct
 
