7f17c3b80145cb16720551108b4c65333ebf3dd7
Karim-Mane <karimanee@outlook.com>
2024-05-21 12:01:48 +0000

    use "%>%" instead of "|>" for harmonisation.

--- R/read_from_dhis2.R
+++ R/read_from_dhis2.R
@@ -84,23 +84,23 @@ read_from_dhis2 <- function(base_url,
   url    <- file.path(base_url, "api", "dataValueSets.json")
 
   # fetching the data of interest
-  data   <- httr2::request(url) |>
-    httr2::req_auth_basic(user_name, password) |>
-    httr2::req_method("GET") |>
-    httr2::req_url_query(!!!query_parameters, .multi = "explode") |>
-    httr2::req_perform() |>
-    httr2::resp_body_json() |>
+  data   <- httr2::request(url) %>%
+    httr2::req_auth_basic(user_name, password) %>%
+    httr2::req_method("GET") %>%
+    httr2::req_url_query(!!!query_parameters, .multi = "explode") %>%
+    httr2::req_perform() %>%
+    httr2::resp_body_json() %>%
     dplyr::bind_rows()
 
   # add the variable names
-  tmp_data_elt <- attributes[["data_elements"]][, c("shortName", "id")] |>
+  tmp_data_elt <- attributes[["data_elements"]][, c("shortName", "id")] %>%
     dplyr::rename("dataElement" = "id") # nolint: keyword_quote_linter
-  tmp_org_units <- attributes[["org_units_details"]][, c("shortName", "id")] |>
+  tmp_org_units <- attributes[["org_units_details"]][, c("shortName", "id")] %>%
     dplyr::rename("orgUnit" = "id") # nolint: keyword_quote_linter
-  data <- data |>
-    dplyr::left_join(tmp_data_elt, by = "dataElement") |>
-    dplyr::rename("dataElementName" = "shortName") |> # nolint: keyword_quote_linter
-    dplyr::left_join(tmp_org_units, by = "orgUnit") |>
+  data <- data %>%
+    dplyr::left_join(tmp_data_elt, by = "dataElement") %>%
+    dplyr::rename("dataElementName" = "shortName") %>% # nolint: keyword_quote_linter
+    dplyr::left_join(tmp_org_units, by = "orgUnit") %>%
     dplyr::rename("OrgUnitName" = "shortName") # nolint: keyword_quote_linter
 
   # subsetting fields
