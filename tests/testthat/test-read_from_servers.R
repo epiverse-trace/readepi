b0cac9792da250a78a79b5ab62ba6348a57714f6
Karim-Mane <karimanee@outlook.com>
2024-09-09 12:19:23 +0000

    remove redundant functions

--- tests/testthat/test-read_from_servers.R
+++ tests/testthat/test-read_from_servers.R
@@ -1,3 +1,36 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("read_server works as expected", {
+  testthat::skip_on_cran()
+  testthat::skip_if_offline()
+
+  # authenticate the user
+  conn <- authenticate(
+    from          = "mysql-rfam-public.ebi.ac.uk",
+    type          = "MySQL",
+    user_name     = "rfamro",
+    password      = "",
+    driver_name   = "",
+    db_name       = "Rfam",
+    port          = 4497
+  )
+
+  # read data from a table where the name is specified in a list of query
+  # parameters
+  authors_list <- read_server(
+    conn  = conn,
+    query = list(table = "author", select = NULL, filter = NULL)
+  )
+  expect_s3_class(authors_list, "data.frame")
+  expect_identical(dim(authors_list), c(119L, 6L))
+  expect_named(authors_list, c("author_id", "name", "last_name", "initials",
+                               "orcid", "synonyms"))
+
+  # read data using query parameters within an SQL query
+  authors_list <- read_server(
+    conn  = conn,
+    query = "select * from author"
+  )
+  expect_s3_class(authors_list, "data.frame")
+  expect_identical(dim(authors_list), c(119L, 6L))
+  expect_named(authors_list, c("author_id", "name", "last_name", "initials",
+                               "orcid", "synonyms"))
 })
