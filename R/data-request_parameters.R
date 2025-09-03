6af7dc2d2a94f2f26e962de5ccbef6779c64dcff
Degoot-AM <degoot@aims.ac.za>
2025-09-03 09:35:34 +0000

    polish documentation of functions

--- R/data-request_parameters.R
+++ R/data-request_parameters.R
@@ -1,9 +1,9 @@
 #' Request parameters
 #'
 #' The request_parameters data frame contains the information related to which
-#' request parameters should be used when fetching data from a specific DHIS2
-#' version of a DHIS2. It currently contains the parameters for version 2.22 to
-#' 2.42.
+#' request parameters should be used when fetching data from a specific
+#' version of  DHIS2. It currently contains parameters for versions ranging
+#' from  2.22 to 2.42.
 #'
 #' @format A data frame of 3 rows and 12 columns
 #' @name request_parameters
